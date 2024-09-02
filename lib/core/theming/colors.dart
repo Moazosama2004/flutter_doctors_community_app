02fc6108734e67298cb3cf6b23efbbb707633bd6
Muaz <102158567+Moazosama2004@users.noreply.github.com>
2024-09-02 15:15:10 +0300

    Revert "Finish login screen. 💯 :"

--- lib/core/theming/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ColorManager {
-  static const Color mainBlue = Color(0xFF247CFF);
-  static const Color maingray = Color(0xFF757575);
-  static const Color lightGray = Color(0xFFFDFDFF);
-  static const Color lighterGray = Color(0xFFC2C2C2);
-  static const Color morelighterGray = Color(0xFF9E9E9E);
-}
